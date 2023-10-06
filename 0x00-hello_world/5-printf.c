b0364698332dfe76fb93c09f49ae9d23f43afafb
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-05 18:07:36 -0700

    Adds a print statement

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
