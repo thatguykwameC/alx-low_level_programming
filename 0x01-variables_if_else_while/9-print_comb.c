76912f66d1baa7f1521837b5d69caec832ceb83a
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-06 21:52:02 -0700

    Prints possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
