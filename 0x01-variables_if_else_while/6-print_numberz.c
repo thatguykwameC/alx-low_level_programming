d79c7882958656c846bde25387184c8ac5b0a06e
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-06 21:16:47 -0700

    Added numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all base 10 digits with only putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+
