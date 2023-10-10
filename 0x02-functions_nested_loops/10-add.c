cf4842f4792a9566f2689e13e5bb56d85c341894
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-10 14:56:06 -0700

    mAdded a sum function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Adds two numbers and returns the result
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: The sum of @a and @b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
