df86eb3d4cb2d560a7aca9ca2fa872745c38ee13
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-12 19:01:27 -0700

    Added a mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: multiplies two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
