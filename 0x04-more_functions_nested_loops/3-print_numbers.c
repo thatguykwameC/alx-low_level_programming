13eefb1735a4b06ae5115ba4b85221855e9640dc
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-12 19:13:06 -0700

    Added a print number function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: Prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
