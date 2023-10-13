e4846c48839cf3d5152febbc8b7d5e710175df5e
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-12 19:24:17 -0700

    Added a number exclusion function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: Prints 0 to 9,
+ * excluding 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num);
+		}
+	}
+}
