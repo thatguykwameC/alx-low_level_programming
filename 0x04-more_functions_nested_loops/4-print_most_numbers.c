2e680cc613f8c42820734ec005ed8321bc8cde75
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-12 19:32:04 -0700

    Edits to function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -16,9 +16,8 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-		{
-			_putchar(num);
-		}
+			continue;
+		_putchar(num);
 	}
 	_putchar('\n');
 }
