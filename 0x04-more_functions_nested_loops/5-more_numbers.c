9f9b4c926903c659211068f918c782bd4c71a64d
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-12 19:53:06 -0700

    Added a count function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Description: Prints the numbers
+ * 0 to 14, ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
