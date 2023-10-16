c09be5de1a6aa69de3dc4f8514abed9deee09429
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-16 13:02:12 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates the value to 98
+ *
+ * @n: An integer
+ *
+ * Return:void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
