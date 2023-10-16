657fd9879931b615e5fda3cf16fea403893ee38b
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-16 14:32:12 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Entry point
+ * Description: Function returns the length of a string
+ * @s: String
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
