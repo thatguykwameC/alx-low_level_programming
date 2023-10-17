3ed127ba7a9a2fffcf11adf64c9dc980903294e8
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-16 18:38:19 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char at even positions
+ * @str: strig to be printed
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[1] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
