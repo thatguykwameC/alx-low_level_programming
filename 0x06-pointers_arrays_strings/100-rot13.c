c845a8fac8052b70a1b53bd41fd8b9046aa38d6c
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-19 04:16:41 -0700

    Task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - perform ROT13 encryption
+ * @s: string to encrypt
+ *
+ * Return: string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	const char *map1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char *map2 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; map1[j] != '\0'; j++)
+		{
+			if (s[i] == map1[j])
+			{
+				s[i] = map2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
