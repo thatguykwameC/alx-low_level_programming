8ecf310743b27a33a13d0fdd3c8483bdcb9af18b
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-28 05:41:47 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - reads two numbers and multiplies them
+ * @argc: arguement count
+ * @argv: arguement vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
