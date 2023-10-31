545204357803766fbceda27fd4c29301eaf6284f
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-31 11:02:21 -0700

    Edits to Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,10 +21,7 @@ int **alloc_grid(int width, int height)
 	grid_2D = malloc(height * sizeof(int *));
 
 	if (grid_2D == NULL)
-	{
-		free(grid_2D);
 		return (NULL);
-	}
 
 	for (i = 0; i < height; i++)
 	{
@@ -32,15 +29,15 @@ int **alloc_grid(int width, int height)
 		if (grid_2D[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
+				free(grid_2D[i]);
+
 			free(grid_2D);
-			free(grid_2D[i]);
 			return (NULL);
 		}
 	}
 
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			grid_2D[i][j] = 0;
+	for (j = 0; j < width; j++)
+		grid_2D[i][j] = 0;
 
 	return (grid_2D);
 }
