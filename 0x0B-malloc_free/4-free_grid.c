195eb3d05f6dbb7acc8be8f8755c07e4f6652918
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-10-31 11:25:39 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees up memory allocated by 2D grid
+ * @grid: 2D grid
+ * @height: Height of grid
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
