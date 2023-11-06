6eaf7bc323091fbc5785d68be81be36b2da5511e
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-06 15:47:35 -0800

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initalizes a variable of type struct dog
+ * @d: pointer to the dog struct
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: name of owner of a dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
