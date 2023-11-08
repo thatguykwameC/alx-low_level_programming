c7754a2be4c89efa9b3cd496b0a2908feb8d06c3
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-08 12:01:48 -0800

    Edits to Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -10,5 +9,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }
