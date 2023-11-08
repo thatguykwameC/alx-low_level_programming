87945d2cfc353625b911f171d9246dc5e9bdf8be
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-08 05:45:40 -0800

    Task o

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: a function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
