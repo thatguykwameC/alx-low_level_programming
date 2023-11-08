6e3dc4d439bce9d18095e3f267b5d1fc452798fc
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-08 12:17:23 -0800

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t j;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (j = 0; j < size; j++)
 		action(array[j]);
 }
