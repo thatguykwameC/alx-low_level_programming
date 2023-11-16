29ab7e33fce9810b596d15fdb07b6c79fd8993de
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-16 13:42:29 -0800

    function that prints a message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_string - prints a string before the main function
+ */
+
+void print_string(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
