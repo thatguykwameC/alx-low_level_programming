10ce65f8824fd7cdecf9bdc73930601b56ee13f8
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-16 11:52:10 -0800

    function to print list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in
+ * a linked list, list_t
+ * @h: the head node
+ *
+ * Return: number of elements in list_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_counter = 0;
+
+	while (h)
+	{
+		node_counter++;
+		h = h->next;
+	}
+	return (node_counter);
+}
