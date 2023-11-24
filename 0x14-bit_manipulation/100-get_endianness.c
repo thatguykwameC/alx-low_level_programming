49448e239b40664843b71ca6ea00e4fd736188fe
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-23 17:58:36 -0800

    Task endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, else 1 for little endian
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+
+	char *endian = (char *)&i;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}
