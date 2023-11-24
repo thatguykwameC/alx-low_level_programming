e41fe97e360f7e95ab2e0e8310712303c2f1e1e6
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-23 18:35:38 -0800

    Edits to Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,16 +12,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bit;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (bit = 0; bit <= 63; n >>= 1, bit++)
-	{
-		if (index == 1)
-			return (n & 1);
-	}
-
-	return (-1);
+	return ((int)((n >> index) & 1));
 }
