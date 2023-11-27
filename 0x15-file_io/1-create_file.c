084cf446c1620d07d0a1f1d5a017ff95f390da23
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-27 12:00:21 -0800

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, 0_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 		return (-1);
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
 		;
-	
+
 	/* Write the content to the file */
 	result = write(file_descriptor, text_content, num_letters);
 
