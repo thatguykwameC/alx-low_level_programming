bfae4a25f353c3ae588ab3453e25db655cd0719d
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-11-27 11:47:30 -0800

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file -  creates a file
+ * @filename: the name of the file to create
+ * @text_content: A NULL terminated string to write to the file
+ *
+ * Return: 1 on success, else -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, num_letters, result;
+
+	if (!filename)
+		return (-1);
+
+	file_descriptor = open(filename, 0_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (num_letters = 0; text_content[num_letters]; num_letters++)
+		;
+	
+	/* Write the content to the file */
+	result = write(file_descriptor, text_content, num_letters);
+
+	if (result == -1)
+		return (-1);
+
+	close(file_descriptor);
+
+	return (1);
+}
